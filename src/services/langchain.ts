9dcfa172569116e4f164d8c8044f3c7312466109
Fernando Mellone <fermellone@gmail.com>
2024-09-11 12:52:29 -0400

    upgrade the embedding model

--- src/services/langchain.ts
+++ src/services/langchain.ts
@@ -34,7 +34,7 @@ export const getEmbeddingModel = () =>
   new OpenAIEmbeddings({
     openAIApiKey: process.env.OPENAI_API_KEY,
     batchSize: 512, // Default value if omitted is 512. Max is 2048
-    modelName: "text-embedding-ada-002",
+    modelName: "text-embedding-3-small",
     maxRetries: 2,
   });
 
